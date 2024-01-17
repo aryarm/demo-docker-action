f6389001307e60e98e02b8304b322cda2c4914e0
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2024-01-17 22:37:21 +0000

    Revert "remove test scripts temporarily"
    This reverts commit 0872a7a1258feb71460e819cd8ca24e19bde5f7c.

---
+++ haptools/test.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+ls environment.yml
+ls hi.txt
+
+echo $PWD
---
+++ trtools/test.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+associaTR --help
+dumpSTR --help
+mergeSTR --help
+statSTR --help
+compareSTR --help
+qcSTR --help
+prancSTR --help
+simTR --help
+python -c "import trtools"
